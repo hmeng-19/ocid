ba9f7bfc2b1cb38ad220d6585d43bebab808d842
Haiyan Meng <hmeng@redhat.com>
2016-07-19 21:30:05 -0400

    Add first version of createPodSandbox
    Signed-off-by: Haiyan Meng <hmeng@redhat.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/kubernetes/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 	"github.com/mrunalp/ocid/server"
+	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 )
 
@@ -14,16 +16,39 @@ const (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	app := cli.NewApp()
+	app.Name = "ocic"
+	app.Usage = "client for ocid"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "sandboxdir",
+			Value: "/var/lib/ocid/sandbox",
+			Usage: "ocid pod sandbox dir",
+		},
 	}
-	s := grpc.NewServer()
-	service, err := server.New("")
-	if err != nil {
+
+	app.Action = func(c *cli.Context) error {
+		lis, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		s := grpc.NewServer()
+
+		sandboxDir := c.String("sandboxdir")
+		service, err := server.New("", sandboxDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		runtime.RegisterRuntimeServiceServer(s, service)
+		runtime.RegisterImageServiceServer(s, service)
+		s.Serve(lis)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-	runtime.RegisterRuntimeServiceServer(s, service)
-	runtime.RegisterImageServiceServer(s, service)
-	s.Serve(lis)
 }
